f41dbd5a7f14156f56e9423fb78f60701da2959e
gurleen227 <55613954+gurleen227@users.noreply.github.com>
2020-07-09 23:32:20 -0700

    Update real_time_object_detection.py

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -2,13 +2,7 @@
 # python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 
 # import the necessary packages
-from imutils.video import VideoStream
-from imutils.video import FPS
-import numpy as np
-import argparse
-import imutils
-import time
-import cv2
+
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
